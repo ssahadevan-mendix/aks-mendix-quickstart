69a722080d693ec944940ab3a39eea90b5b85d54
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2023-04-27 10:33:27 -0500

    Unique cluster name
    Make cluster name unique

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "default" {
 
 resource "azurerm_kubernetes_cluster" "default" {
   #name                = "${random_pet.prefix.id}-aks"
-  name                = "${var.clusterName}"
+  name                = "${var.clusterName}-${random_pet.prefix.id}"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
