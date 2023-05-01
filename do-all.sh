2d06baccb2600dfc362876f299164c69bf19cf8b
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2023-05-01 11:24:41 -0500

    Added Standalone
    Enhanced to perform standalone.

--- do-all.sh
+++ do-all.sh
@@ -9,6 +9,9 @@ chmod +x *.sh
 
 . ./get-creds.sh
 
+defaultMode="connected"
+echo "Mode is $mode"
+
 
 echo https://cdn.mendix.com/mendix-for-private-cloud/mxpc-cli/mxpc-cli-$mendixOperatorVersion-$os.tar.gz
 wget https://cdn.mendix.com/mendix-for-private-cloud/mxpc-cli/mxpc-cli-$mendixOperatorVersion-$os.tar.gz
@@ -27,8 +30,19 @@ helm repo update
 . ./generate-yamls.sh
 
 
-./mxpc-cli base-install --namespace $MENDIX_NAMESPACE -i $MENDIX_CONFIG_IVAL -s $MENDIX_CONFIG_SVAL --clusterMode connected --clusterType generic
-./mxpc-cli apply-config -i $MENDIX_CONFIG_IVAL -s $MENDIX_CONFIG_SVAL --file configure.yaml
+if [ $mode == $defaultMode ];
+then
+	echo "Base install "
+        ./mxpc-cli base-install --namespace $MENDIX_NAMESPACE -i $MENDIX_CONFIG_IVAL -s $MENDIX_CONFIG_SVAL --clusterMode connected --clusterType generic
+	echo "Apply Config "
+        ./mxpc-cli apply-config -i $MENDIX_CONFIG_IVAL -s $MENDIX_CONFIG_SVAL --file configure.yaml
+else
+	 echo "base install "
+ 	 ./mxpc-cli base-install --namespace $MENDIX_NAMESPACE  --clusterMode standalone  --clusterType generic
+
+  	echo "apply config"
+  	./mxpc-cli apply-config  --file configure-standalone.yaml
+fi
 
 
 ### Deploy the application
--- main.tf
+++ main.tf
@@ -49,8 +49,6 @@ resource "azurerm_kubernetes_cluster" "default" {
 
 provider "kubernetes" {
   host                   = "${azurerm_kubernetes_cluster.default.kube_config.0.host}"
-  #username               = "${azurerm_kubernetes_cluster.default.kube_config.0.username}"
-  #password               = "${azurerm_kubernetes_cluster.default.kube_config.0.password}"
   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)}"
   client_key             = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)}"
@@ -60,8 +58,6 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
   host                   = "${azurerm_kubernetes_cluster.default.kube_config.0.host}"
-  #username               = "${azurerm_kubernetes_cluster.default.kube_config.0.username}"
-  #password               = "${azurerm_kubernetes_cluster.default.kube_config.0.password}"
   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)}"
   client_key             = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)}"
