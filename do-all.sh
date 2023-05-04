1021b06821999a5694930d2d5543c7fb54eb17c7
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2023-05-04 11:26:20 -0500

    Update do-all.sh
    Removed helm repo add.

--- do-all.sh
+++ do-all.sh
@@ -19,12 +19,6 @@ tar xvf mxpc-cli-$mendixOperatorVersion-$os.tar.gz
 
 rm mxpc-cli-$mendixOperatorVersion-$os.tar.gz*
 
-helm repo add stable https://charts.helm.sh/stable
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-
 . ./install-grafana-prometheus.sh
 
 . ./generate-yamls.sh
